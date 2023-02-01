5a1dbb014c558fdf4091661575a9dfd508e4345a
BeulaSamantha <95486339+samBeula@users.noreply.github.com>
2023-02-01 21:31:36 +0530

    Add files via upload

---
+++ git exp/src/com/demo/Arithmetic2.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+public class Arithmetic2 {
+    public int add(int a, int b){
+        return a+b;
+    }
+    public int sub(int a, int b){
+        return a-b;
+    }
+    public int mul(int a, int b){
+        return a*b;
+    }
+    public int div(int a, int b){
+        return a/b;
+    }
+}
+
+
