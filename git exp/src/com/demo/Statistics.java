0f9fc384aab15d493bc657fa951ac67bda5fab3e
BeulaSamantha <95486339+samBeula@users.noreply.github.com>
2023-02-01 21:32:12 +0530

    Add files via upload

---
+++ git exp/src/com/demo/Statistics.java
@@ -0,0 +1,10 @@
+package com.demo;
+
+public class Statistics extends Arithmetic2{
+    public float mean(int a,int b, int c, int d){
+        return (a+b+c+d)/4;
+    }
+    public float median(int a,int b, int c, int d){
+        return (b+c)/2;
+    }
+}
