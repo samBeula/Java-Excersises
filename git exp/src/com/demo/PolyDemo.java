7270c5ed4ef5ec8146e1594e9c0030adf4df0b6a
BeulaSamantha <95486339+samBeula@users.noreply.github.com>
2023-02-02 12:59:28 +0530

    Add files via upload

---
+++ git exp/src/com/demo/PolyDemo.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+public class PolyDemo {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eats();
+        dog.sleeps();
+        Cow cow = new Cow();
+        cow.eats();
+        cow.sleeps();
+    }
+}
