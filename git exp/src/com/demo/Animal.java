d2f5a540c91f7e3257d4fc148e205b57325346ae
BeulaSamantha <95486339+samBeula@users.noreply.github.com>
2023-02-02 12:57:54 +0530

    Add files via upload

---
+++ git exp/src/com/demo/Animal.java
@@ -0,0 +1,10 @@
+package com.demo;
+
+public class Animal {
+    public void eats(){
+        System.out.println("Eats food");
+    }
+    public void sleeps(){
+        System.out.println("Animals need rest");
+    }
+}
